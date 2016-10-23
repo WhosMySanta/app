2dfe0a9a48d4901f4ae26d4a8ff4214f9a2a34d8
Karl Horky <karl.horky@gmail.com>
2016-10-23 12:09:22 +0200

    Scaffold out transpilation, linting, typechecking

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    jest: true,
+    node: true
+  },
+  plugins: [
+    'flowtype'
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    // 'max-len': 'off'
+    'flowtype/boolean-style': 'error',
+    'flowtype/define-flow-type': 'error',
+    'flowtype/delimiter-dangle': ['error', 'always-multiline'],
+    'flowtype/generic-spacing': 'error',
+    'flowtype/no-weak-types': 'error',
+    'flowtype/require-parameter-type': 'off',
+    'flowtype/require-return-type': 'off',
+    'flowtype/require-valid-file-annotation': 'error',
+    'flowtype/semi': 'error',
+    'flowtype/space-after-type-colon': 'error',
+    'flowtype/space-before-generic-bracket': 'error',
+    'flowtype/space-before-type-colon': 'error',
+    'flowtype/type-id-match': ['error', '^[A-Z][a-z]+(?:[A-Z][a-z]+)*$'],
+    'flowtype/union-intersection-spacing': 'error',
+    'flowtype/use-flow-type': 'error',
+    'flowtype/valid-syntax': 'error'
+  }
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+/* @flow */
+// import a from './a';
+
+const doSomething = (ai: number) => {
+  console.log(ai + 1);
+};
+
+doSomething('a');
