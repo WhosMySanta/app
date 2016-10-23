bea075ca33f8237749e03bf1a9a02f8c86e54917
Karl Horky <karl.horky@gmail.com>
2016-10-23 12:45:32 +0200

    Start adding types

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 /* @flow */
+
+type GiftCategory =
+  'Electronics' |
+  'Games' |
+  'Home Decoration';
+
+type GiftWish = string | Array<GiftCategory>;
+
+type User = {
+  firstName: string,
+  lastName?: string,
+  giftWish?: GiftWish,
+};
+
+type Selection = {
+  gifter: User,
+  giftee: User,
+  giftWish?: GiftWish,
+};
+
+type SelectSecretSantaFn = (users: Array<User>) => Array<?Selection>;
+
+const selectSecretSanta = (users): SelectSecretSantaFn => {
+  users.forEach(u => console.log(u));
+  return [];
+};
+
+export default selectSecretSanta;
