b8b9a995337cd25e05342e80cc95c57434d33b26
Glenn Reyes <glenn@glennreyes.com>
2016-11-09 20:29:38 +0100

    Remove unused commented stuff and log it super verbose

--- server/config.dev.js
+++ server/config.dev.js
@@ -6,35 +6,18 @@ const { join } = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
-// const clearConsole = require('react-dev-utils/clearConsole');
 
 const config = require('../config/webpack.config.dev');
-
-// const path = require('path');
-
-// const config = {
-//   entry: path.resolve(__dirname, '..', 'src', 'index.js'),
-//   module: {
-//     loaders: [
-//       {
-//         exclude: /node_modules/,
-//         loader: 'babel',
-//         test: /\.js$/,
-//       },
-//     ],
-//   },
-//   output: { filename: '/app.js', path: '/', publicPath: '/js/' },
-// };
 
 export default ({ app, port }) => {
   const compiler = webpack(config);
   const middleware = webpackDevMiddleware(compiler, {
     contentBase: 'public',
+    noInfo: true,
     publicPath: config.output.publicPath,
     port,
     stats: {
       colors: true,
-      noInfo: true,
       progress: true,
       timings: true,
     },
