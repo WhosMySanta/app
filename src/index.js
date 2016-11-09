a347ac166824cbda62f0ed6cad4232c6c42ab1b1
Karl Horky <karl.horky@gmail.com>
2016-11-09 19:09:59 +0100

    Pass raffleId from url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Match } from 'react-router';
 
 import App from './components/App';
 
@@ -16,12 +16,12 @@ class AppRoute extends Relay.Route {
   static paramDefinitions = {
     // By setting `required` to true, `ProfileRoute` will throw if a `userID`
     // is not supplied when instantiated.
-    raffleID: { required: true },
+    raffleId: { required: true },
   };
   static queries = {
     raffleGroup: (Component) => Relay.QL`
       query RaffleGroupQuery {
-        raffleGroup(id: $raffleID) { ${Component.getFragment('raffleGroup')} },
+        raffleGroup(id: $raffleId) { ${Component.getFragment('raffleGroup')} },
       }
     `,
   };
@@ -29,9 +29,14 @@ class AppRoute extends Relay.Route {
 
 ReactDOM.render(
   <BrowserRouter>
-    <Relay.RootContainer
-      Component={App}
-      route={new AppRoute({ raffleID: '123a' })}
+    <Match
+      pattern="/raffle/:raffleId"
+      render={({ params: { raffleId } }) =>
+        <Relay.RootContainer
+          Component={App}
+          route={new AppRoute({ raffleId })}
+        />
+      }
     />
   </BrowserRouter>,
   document.getElementById('root'),
