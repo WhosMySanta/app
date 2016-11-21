f9ac42c88346f31cbc49ca9f3b3cfb0a1d2d63df
Glenn Reyes <glenn@glennreyes.com>
2016-11-21 21:11:33 +0100

    Extract friends textfield from Create component

---
+++ src/components/TextField/index.jsx
@@ -0,0 +1,35 @@
+/* @flow */
+
+import React from 'react';
+
+import type {HandleChangeEvent} from '../Create';
+
+
+type Props = {
+  id: string,
+  label?: string,
+  onChange: HandleChangeEvent,
+  type?: 'email' | 'text',
+  value?: string,
+};
+
+const TextField = ({
+  id,
+  label,
+  onChange,
+  type = 'text',
+  value,
+}: Props) => (
+  <span>
+    {label && <label htmlFor={id}>{label}</label>}
+    <input
+      type={type}
+      id={id}
+      value={value}
+      onChange={onChange}
+    />
+  </span>
+);
+
+
+export default TextField;
