3fe51fb95398c667d3630440726a9cfeef0543b0
Glenn Reyes <glenn@glennreyes.com>
2016-11-28 21:31:46 +0100

    Controlled components and simple typings

--- src/components/Wish/index.jsx
+++ src/components/Wish/index.jsx
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, {Component} from 'react';
-import Relay, {createContainer} from 'react-relay';
+import Relay, {createContainer, Mutation, Store} from 'react-relay';
 
 import TextField from '../TextField';
 
@@ -20,47 +20,53 @@ type State = {
   wish: string,
 };
 
-// class WishMutation extends Mutation {
-//   static fragments = {
-//     app: () => Relay.QL`
-//       fragment on App {
-//         id,
-//       },
-//     `,
-//   }
-//   getMutation() {
-//     return Relay.QL`
-//       mutation { updateWish },
-//     `;
-//   }
-//   getVariables() {
-//     const {email, wish} = this.props.friend;
-//
-//     return {
-//       email,
-//       wish,
-//     };
-//   }
-//   getFatQuery() {
-//     return Relay.QL`
-//       fragment on UpdateWishPayload {
-//         app {
-//           group(id: $groupId) {
-//             friends,
-//           }
-//         }
-//       },
-//     `;
-//   }
-//   getConfigs() {
-//     return [
-//       {
-//         type: 'FIELDS_CHANGE',
-//         fieldIDs: {app: this.props.app.id},
-//       },
-//     ];
-//   }
-// }
+type OnChangeEmailFn = (event: {target: {value: string}}) => void;
+
+type OnChangeWishFn = (event: {target: {value: string}}) => void;
+
+type OnSubmitFn = () => void;
+
+class UpdateWishMutation extends Mutation {
+  static fragments = {
+    app: () => Relay.QL`
+      fragment on App {
+        id,
+      },
+    `,
+  }
+  getMutation() {
+    return Relay.QL`
+      mutation { updateWish },
+    `;
+  }
+  getVariables() {
+    const {email, wish} = this.props.friend;
+
+    return {
+      email,
+      wish,
+    };
+  }
+  getFatQuery() {
+    return Relay.QL`
+      fragment on UpdateWishPayload {
+        app {
+          group(id: $groupId) {
+            friends,
+          }
+        }
+      },
+    `;
+  }
+  getConfigs() {
+    return [
+      {
+        type: 'FIELDS_CHANGE',
+        fieldIDs: {app: this.props.app.id},
+      },
+    ];
+  }
+}
 
 class Wish extends Component {
   state: State = {
@@ -68,8 +74,34 @@ class Wish extends Component {
     wish: '',
   }
 
+
+  onChangeEmail: OnChangeEmailFn = ({target: {value}}) => {
+    this.setState({email: value});
+  }
+
+  onChangeWish: OnChangeWishFn = ({target: {value}}) => {
+    this.setState({wish: value});
+  }
+
+  onSubmit: OnSubmitFn = () => {
+    const {app} = this.props;
+    const {email, wish} = this.state;
+
+    Store.commitUpdate(
+      new UpdateWishMutation({
+        app,
+        friend: {
+          email,
+          wish,
+        },
+      }),
+    );
+  }
+
   props: Props
+
   render() {
+    const {onChangeEmail, onChangeWish, onSubmit} = this;
     const {app: {group: {title, description}}} = this.props;
     const {email, wish} = this.state;
 
@@ -88,18 +120,20 @@ class Wish extends Component {
               type="email"
               id="email"
               value={email}
+              onChange={onChangeEmail}
             />
             <label htmlFor="wish">What would you like?</label>
             <textarea
               id="wish"
               name="wish"
               placeholder="New iPhone, Home Decoration, Electronics"
-              defaultValue={wish}
+              value={wish}
+              onChange={onChangeWish}
             />
           </div>
         </section>
 
-        <button type="button">Send</button>
+        <button type="button" onClick={onSubmit}>Send</button>
       </main>
     );
   }
